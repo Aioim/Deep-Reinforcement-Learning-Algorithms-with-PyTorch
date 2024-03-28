ad94f6732c65b249f4025f00f323ffa6774adaaf
Aioim <178754558@qq.com>
2024-03-28 22:24:22 +0800

    Update DQN.py

--- agents/DQN_agents/DQN.py
+++ agents/DQN_agents/DQN.py
@@ -9,50 +9,58 @@ from agents.Base_Agent import Base_Agent
 from exploration_strategies.Epsilon_Greedy_Exploration import Epsilon_Greedy_Exploration
 from utilities.data_structures.Replay_Buffer import Replay_Buffer
 
-
 class DQN(Base_Agent):
     """A deep Q learning agent"""
     agent_name = "DQN"
-
     def __init__(self, config):
         Base_Agent.__init__(self, config)
-        self.memory = Replay_Buffer(self.hyperparameters["buffer_size"], self.hyperparameters["batch_size"],
-                                    config.seed, self.device)
+        self.memory = Replay_Buffer(self.hyperparameters["buffer_size"], self.hyperparameters["batch_size"], config.seed, self.device)
         self.q_network_local = self.create_NN(input_dim=self.state_size, output_dim=self.action_size)
         self.q_network_optimizer = optim.Adam(self.q_network_local.parameters(),
                                               lr=self.hyperparameters["learning_rate"], eps=1e-4)
+        # 动作的探索与利用策略
         self.exploration_strategy = Epsilon_Greedy_Exploration(config)
 
     def reset_game(self):
         super(DQN, self).reset_game()
+        # 更新学习率
         self.update_learning_rate(self.hyperparameters["learning_rate"], self.q_network_optimizer)
 
     def step(self):
         """Runs a step within a game including a learning step if required"""
         while not self.done:
+            # 选择动作
             self.action = self.pick_action()
+            # 执行动作
             self.conduct_action(self.action)
+            # 是否达到学习条件
             if self.time_for_q_network_to_learn():
                 for _ in range(self.hyperparameters["learning_iterations"]):
                     self.learn()
+            # 保存经验
             self.save_experience()
-            self.state = self.next_state  # this is to set the state for the next iteration
+            self.state = self.next_state #this is to set the state for the next iteration
             self.global_step_number += 1
         self.episode_number += 1
 
     def pick_action(self, state=None):
+        # 选择动作
         """Uses the local Q network and an epsilon greedy policy to pick an action"""
         # PyTorch only accepts mini-batches and not single observations so we have to use unsqueeze to add
         # a "fake" dimension to make it a mini-batch rather than a single observation
         if state is None: state = self.state
         if isinstance(state, np.int64) or isinstance(state, int): state = np.array([state])
         state = torch.from_numpy(state).float().unsqueeze(0).to(self.device)
-        # 如果数据维度小于2，则增维
+        # 如果数据只有一维，则增加到2维,shape:1,len(state)
         if len(state.shape) < 2: state = state.unsqueeze(0)
-        self.q_network_local.eval()  # puts network in evaluation mode
+        self.q_network_local.eval() #puts network in evaluation mode
         with torch.no_grad():
             action_values = self.q_network_local(state)
-        self.q_network_local.train()  # puts network back in training mode
+        self.q_network_local.train() #puts network back in training mode
+        # 动作获取策略
+        # 1、通过model获取的actions信息，所有动作的值信息
+        # 2、turn_off_exploration 是否关闭探索
+        # 3、当前幕  
         action = self.exploration_strategy.perturb_action_for_exploration_purposes({"action_values": action_values,
                                                                                     "turn_off_exploration": self.turn_off_exploration,
                                                                                     "episode_number": self.episode_number})
@@ -60,18 +68,16 @@ class DQN(Base_Agent):
         return action
 
     def learn(self, experiences=None):
+        
         """Runs a learning iteration for the Q network"""
-        if experiences is None:
-            states, actions, rewards, next_states, dones = self.sample_experiences()  # Sample experiences
-        else:
-            states, actions, rewards, next_states, dones = experiences
+        if experiences is None: states, actions, rewards, next_states, dones = self.sample_experiences() #Sample experiences
+        else: states, actions, rewards, next_states, dones = experiences
         loss = self.compute_loss(states, next_states, rewards, actions, dones)
 
-        actions_list = [action_X.item() for action_X in actions]
+        actions_list = [action_X.item() for action_X in actions ]
 
         self.logger.info("Action counts {}".format(Counter(actions_list)))
-        self.take_optimisation_step(self.q_network_optimizer, self.q_network_local, loss,
-                                    self.hyperparameters["gradient_clipping_norm"])
+        self.take_optimisation_step(self.q_network_optimizer, self.q_network_local, loss, self.hyperparameters["gradient_clipping_norm"])
 
     def compute_loss(self, states, next_states, rewards, actions, dones):
         # 计算损失
@@ -82,8 +88,9 @@ class DQN(Base_Agent):
         loss = F.mse_loss(Q_expected, Q_targets)
         return loss
 
+
     def compute_q_targets(self, next_states, rewards, dones):
-        # 计算指定状态的Q值
+        #计算指定状态的Q值
         """Computes the q_targets we will compare to predicted q values to create the loss to train the Q network"""
         Q_targets_next = self.compute_q_values_for_next_states(next_states)
         Q_targets = self.compute_q_values_for_current_states(rewards, Q_targets_next, dones)
@@ -104,8 +111,7 @@ class DQN(Base_Agent):
     def compute_expected_q_values(self, states, actions):
         # 计算Q的期望值
         """Computes the expected q_values we will use to create the loss to train the Q network"""
-        Q_expected = self.q_network_local(states).gather(1,
-                                                         actions.long())  # must convert actions to long so can be used as index
+        Q_expected = self.q_network_local(states).gather(1, actions.long()) #must convert actions to long so can be used as index
         return Q_expected
 
     def locally_save_policy(self):
